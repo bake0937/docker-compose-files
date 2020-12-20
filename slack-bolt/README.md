7b687491b2a4bbe9efcb0dd041ea8d81309c2596
bake0937 <sadayasu.info@gmail.com>
2020-12-20 20:46:13 +0900

    Added Docker for Bolt

---
+++ slack-bolt/README.md
@@ -0,0 +1,11 @@
+.envを作成し，以下を設定する
+```bash
+SLACK_SIGNING_SECRET=
+SLACK_BOT_TOKEN=
+```
+
+以下を実行する
+```bash
+$docker-compose build
+$docker-compose up
+```
---
+++ slack-bolt/app.js
@@ -0,0 +1,23 @@
+const { App } = require('@slack/bolt');
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
+});
+
+// Listens to incoming messages that contain "hello"
+app.message('hello', async ({ message, say }) => {
+  try {
+    // say() sends a message to the channel where the event was triggered
+    await say(`Hey there <@${message.user}>!`);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+(async () => {
+  // Start your app
+  await app.start(process.env.PORT || 3000);
+
+  console.log('⚡️ Bolt app is running!');
+})();
